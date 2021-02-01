701ca2053fd2a92a4123793c2a3410daaa8ea953
Nischal Rai <nischalpilmong@yahoo.com>
2021-02-01 22:51:38 +0100

    set the value of the item in local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Alert from './Alert';
 import List from './List'
 
 
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'));
+  }else{
+    return [];
+  }
+}
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [editID, setEditID] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
@@ -62,6 +71,10 @@ function App() {
        setEditID(id);
        setName(specificItem.title);
   };
+   //Setting local storage
+    useEffect(() => {
+      localStorage.setItem('list', JSON.stringify(list));
+     }, [list]);
 
   return (
     <section className="section-center">
