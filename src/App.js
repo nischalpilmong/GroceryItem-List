d86428d4b8b240e5377804738c4416c54b0b72f5
Nischal Rai <nischalpilmong@yahoo.com>
2021-02-01 12:09:46 +0100

    added grocery form structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Alert from './Alert';
+import List from './List'
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('Hello');
+  };
   return (
-    <div className="App">
+    <section className="section-center">
        <h1>Grocery List</h1>
-    </div>
+       <form className="grocery-form" onSubmit={handleSubmit}>
+         {alert.show && <Alert />}
+         <h3>Grocery Item</h3>
+         <div className="form-control">
+           <input type="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e) => setName(e.target.value)}/>
+           <button type="submit" className="submit-btn">
+             {isEditing ? 'edit' : 'submit'}
+           </button>
+         </div>
+       </form>
+       <div className="grocery-container">
+         <List />
+         <button className="clear-btn">Clear items</button>
+       </div>
+    </section>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const List = () => {
+   return(
+        <div>
+          <h2>List Component</h2>
+        </div>
+   );
+}
+export default List;
